0e208398fbefa8a9ce9e4272f305a1e1f8ee7918
Manu254rce <straussmoabit@gmail.com>
2024-02-19 23:23:22 +0300

    Completed task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,19 +10,25 @@
 void selection_sort(int *array, size_t size)
 {
 	int temp;
-	size_t i, j;
+	size_t i, j, min_idx;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		min_idx = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[i])
+			if (array[j] < array[min_idx])
 			{
-				temp = array[j];
-				array[j] = array[i];
-				array[i] = temp;
-				print_array(array, size);
+				min_idx = j;
 			}
+		}
+
+		if (min_idx != i)
+		{
+			temp = array[min_idx];
+			array[min_idx] = array[i];
+			array[i] = temp;
+			print_array(array, size);
 		}
 	}
 }
